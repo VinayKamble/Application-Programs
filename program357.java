e75a6a9cf67525bf6852f7e56b5882c8a83d995d
VinayKamble <132741240+VinayKamble@users.noreply.github.com>
2023-07-21 12:30:33 +0530

    Add files via upload

---
+++ program357.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class StringX
+{
+    public String StrLower(String str)
+    {
+        char Arr[] = str.toCharArray();
+
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if((Arr[iCnt] >= 'A') && (Arr[iCnt] <= 'Z'))
+            {
+                Arr[iCnt] = (char)(Arr[iCnt] + 32);
+            }
+        }
+
+        String ret = new String(Arr);
+
+        return ret;
+    }
+    
+    public String StrUpper(String str)
+    {
+        char Arr[] = str.toCharArray();
+
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if((Arr[iCnt] >= 'a') && (Arr[iCnt] <= 'z'))
+            {
+                Arr[iCnt] = (char)(Arr[iCnt] - 32);
+            }
+        }
+
+        String ret = new String(Arr);
+
+        return ret;
+    }
+}
+
+class program356
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sobj.nextLine();
+
+        StringX obj = new StringX();
+        
+        String sret = obj.StrLower(name);
+        System.out.println("Converted string is : "+sret);
+    }
+}
---
+++ program358.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+class StringX
+{
+    public String StrLower(String str)
+    {
+        char Arr[] = str.toCharArray();
+
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if((Arr[iCnt] >= 'A') && (Arr[iCnt] <= 'Z'))
+            {
+                Arr[iCnt] = (char)(Arr[iCnt] + 32);
+            }
+        }
+
+        String ret = new String(Arr);
+
+        return ret;
+    }
+    
+    public String StrUpper(String str)
+    {
+        char Arr[] = str.toCharArray();
+
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if((Arr[iCnt] >= 'a') && (Arr[iCnt] <= 'z'))
+            {
+                Arr[iCnt] = (char)(Arr[iCnt] - 32);
+            }
+        }
+
+        String ret = new String(Arr);
+
+        return ret;
+    }
+
+    public String StrToggle(String str)
+    {
+        char Arr[] = str.toCharArray();
+
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if((Arr[iCnt] >= 'a') && (Arr[iCnt] <= 'z'))
+            {
+                Arr[iCnt] = (char)(Arr[iCnt] - 32);
+            }
+            else if((Arr[iCnt] >= 'A') && (Arr[iCnt] <= 'Z'))
+            {
+                Arr[iCnt] = (char)(Arr[iCnt] + 32);
+            }
+        }
+
+        String ret = new String(Arr);
+
+        return ret;
+    }
+}
+
+class program358
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sobj.nextLine();
+
+        StringX obj = new StringX();
+        
+        String sret = obj.StrLower(name);
+        System.out.println("Converted string is : "+sret);
+
+        sret = obj.StrUpper(name);
+        System.out.println("Converted string is : "+sret);
+
+        sret = obj.StrToggle(name);
+        System.out.println("Converted string is : "+sret);
+    }
+}
